e662405ae496ec126bf3d11e6b694ea81cc7027a
EduardSuyargulov <edsuyargulov@gmail.com>
2026-04-26 15:59:02 +0500

    sending gpt prompt

---
+++ main.py
@@ -0,0 +1,36 @@
+import os
+from dotenv import load_dotenv
+import requests
+import openai
+
+url = 'https://habr.com/ru/feed/'
+
+url_response = requests.get(url)
+
+if url_response.status_code == 200:
+    print("200 OK")
+else:
+    print(f"Request failed with status code: {url_response.status_code}")
+
+
+load_dotenv()
+
+YANDEX_CLOUD_FOLDER = os.getenv("YANDEX_CLOUD_FOLDER")
+YANDEX_CLOUD_API_KEY = os.getenv("YANDEX_CLOUD_API_KEY")
+YANDEX_CLOUD_MODEL = os.getenv("YANDEX_CLOUD_MODEL")
+
+client = openai.OpenAI(
+  api_key=YANDEX_CLOUD_API_KEY,
+  base_url="https://ai.api.cloud.yandex.net/v1",
+  project=YANDEX_CLOUD_FOLDER
+)
+
+response = client.responses.create(
+  model=f"gpt://{YANDEX_CLOUD_FOLDER}/{YANDEX_CLOUD_MODEL}",
+  temperature=0.3,
+  instructions="Отвечай кратко и по-русски. Как ты должен понимать важность информации: пиши о событиях, которые действительно случились. Пиши о законах, которые приняты, а не просто обсуждаются.",
+  input=f"Составь дайджест с этой страницы {url_response.text[:1000]}, выдели оттуда важную информацию.",
+  max_output_tokens=500
+)
+
+print(response.output_text)
